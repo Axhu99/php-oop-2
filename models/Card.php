2a87bdad5c2305cf91bee2ee83aba568c9ca4bc0
Axhu99 <xhuliodreni@icloud.com>
2024-02-26 19:02:24 +0100

    added class card

---
+++ models/Card.php
@@ -0,0 +1,48 @@
+<?php
+
+include_once __DIR__ . '/Animal.php';
+include_once __DIR__ . '/Product.php';
+
+class Card
+{
+    private $name_brand;
+    private $name_product;
+    private $price_product;
+
+    public function __construct(Product $card, /* Animal $animal */)
+    {
+        $this->setNameBrand($card);
+        $this->setNameProduct($card);
+        $this->setPriceProduct($card);
+    }
+
+    public function getNameBrand()
+    {
+        return $this->name_brand;
+    }
+
+    private function setNameBrand(Product $card)
+    {
+        $this->name_brand = $card->getName();
+    }
+
+    public function getNameProduct()
+    {
+        return $this->name_product;
+    }
+
+    private function setNameProduct(Product $card)
+    {
+        $this->name_product = $card->getBrand();
+    }
+
+    public function getPriceProduct()
+    {
+        return $this->price_product;
+    }
+
+    private function setPriceProduct(Product $card)
+    {
+        $this->price_product = $card->getPrice();
+    }
+}
